782c881823d8ee92bd35f97b87a67a30c244161d
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2024-04-03 16:28:10 -0400

    Schemas added to system

--- SearchApp/admin.py
+++ SearchApp/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Dividends, Institutionalholders, Majorholders, Saveddata, SiteUser, Stock
 
 # Register your models here.
+admin.site.register(Stock)
+admin.site.register(SiteUser)
 admin.site.register(Dividends)
 admin.site.register(Institutionalholders)
 admin.site.register(Majorholders)
 admin.site.register(Saveddata)
-admin.site.register(SiteUser)
-admin.site.register(Stock)
