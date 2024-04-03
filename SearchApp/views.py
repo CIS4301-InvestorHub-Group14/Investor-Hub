5dc4fe592ed4dafb5ef40f1c4120dc1bcc8099bf
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2024-04-03 14:24:48 -0400

    Initial commit

---
+++ SearchApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+# You can find all the html files in the templates file
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+# Index will be our welcome page
+def index_view(request):
+    return render(request, 'index.html')
+
+def home_view(request):
+    return render(request, 'dashboard.html')
